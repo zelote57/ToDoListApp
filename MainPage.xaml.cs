9e0c26599a5e044c010784e2ba42b6911cf7a2ec
Gustavo Lozada <zelote57@gmail.com>
2023-10-24 21:09:05 -0500

    Binding

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,10 +1,48 @@
-﻿namespace ToDoListApp
+﻿using System.Collections.ObjectModel;
+
+namespace ToDoListApp
 {
     public partial class MainPage : ContentPage
     {
+        public ObservableCollection<Tasks> MyTasks { get; set; }
+        public string MyTitle { get; set; } = "Lista de Tareas";
         public MainPage()
         {
             InitializeComponent();
-        }        
+            BindingContext = this;            
+        }
+
+        private void CreateAndLoadTaks()
+        {
+            MyTasks = new ObservableCollection<Tasks>
+            {
+                new Tasks
+                {
+                    TaskName = "Pasear al Perro",
+                    Status = "Pendiente"
+                },
+                new Tasks
+                {
+                    TaskName = "Salir a Correr",
+                    Status = "Pendiente"
+                },
+                new Tasks
+                {
+                    TaskName = "Entregar Tareas U",
+                    Status = "Completada"
+                }
+            };
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            CreateAndLoadTaks();
+        }
+
+        private void AddBtn_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Alert", "Agregado", "OK");
+        }
     }
 }
---
+++ Tasks.cs
@@ -0,0 +1,8 @@
+﻿namespace ToDoListApp
+{
+    public class Tasks
+    {
+        public string TaskName { get; set; }
+        public string Status { get; set; }
+    }
+}
